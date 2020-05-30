673eb2678815b4bf81acee7b8adbec1540c1efbe
lvgraham <liavibanez@gmail.com>
2020-05-30 17:56:01 -0500

    adding temperature info for searched city

--- app.js
+++ app.js
@@ -7,23 +7,56 @@ searchBtn.addEventListener("click", function(){
 
     event.preventDefault();
 
+    //setting city name from value entered in search box
     let city = cityName.value;
-    console.log(city);
 
+    //setting apikey variable
     const APIKey = "d367dcfadab5440b10dca09382825e01";
+    //setting query URL value
     const queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
 
+    //ajax request to open weather
     $.ajax({
         url:queryURL,
         method:"GET"
     }).then(function(response){
-        console.log(queryURL);
         console.log(response);
 
-        $(".city").html('<h1>' + response.name + ' Weather Details</h1>');
-        $(".wind").text('Wind speed: ' + response.wind.speed);
-        $(".humidity").text('Humidity: ' + response.main.humidity);
+        //using moment.js to get current date
+        let now = moment();
+        //converting kelvin temp to farenheight
+        let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+        //adding city, temp, humidity, wind, and UV
+        $(".city").html('<h2>' + response.name + " (" + now.format('MM-DD-YYYY') + ") </h2>");
+        $(".tempF").text('Temperature (F): ' + tempF.toFixed(2));
+        $(".humidity").text('Humidity: ' + response.main.humidity + "%");
+        $(".wind").text('Wind speed: ' + response.wind.speed + " MPH");
+        $(".UV").text('UV Index: ');
     })
+
+    const queryURL2 = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey;
+    console.log(queryURL2);
+
+    // $.ajax({
+    //     url:queryURL,
+    //     method:"GET"
+    // }).then(function(response){
+    //     console.log(response);
+
+    //     //using moment.js to get current date
+    //     let now = moment();
+    //     //converting kelvin temp to farenheight
+    //     let tempF = (response.main.temp - 273.15) * 1.80 + 32;
+
+    //     //adding city, temp, humidity, wind, and UV
+    //     $(".city").html('<h2>' + response.name + " (" + now.format('MM-DD-YYYY') + ") </h2>");
+    //     $(".tempF").text('Temperature (F): ' + tempF.toFixed(2));
+    //     $(".humidity").text('Humidity: ' + response.main.humidity + "%");
+    //     $(".wind").text('Wind speed: ' + response.wind.speed + " MPH");
+    //     $(".UV").text('UV Index: ');
+    // })
+
 
 });
 
